e1a395d20a3334505a1ce29ccc0d634edd98cbd5
ramyavs <31505005+ramyavs@users.noreply.github.com>
2017-09-12 16:14:55 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void nain()
+{
+int n;
+printf("enter n numbers");
+scanf("%d",&n);
+if(n>=0)
+{
+printf("it is a positive number");
+else
+printf("it is a negative number");
+}}
