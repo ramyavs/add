05887c60a6937bf51b9f9606249f609404cb692d
ramyavs <31505005+ramyavs@users.noreply.github.com>
2017-09-12 16:25:54 +0530

    Update and rename add.c to oddeven.c

---
+++ oddeven.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the n numbers");
+scanf(:%d",&n);
+if (n%2==0)
+{
+printf ("it is an 0dd number");
+else 
+printf("it is a even number");
+}}
